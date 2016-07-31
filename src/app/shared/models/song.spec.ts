2c21f039a149267af712b3785dfbbd504b017231
git <briant.dev@gmail.com>
2016-07-31 15:45:33 -0500

    songs to be added to playlists

---
+++ src/app/shared/models/song.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Song} from './song';
+
+describe('Song', () => {
+  it('should create an instance', () => {
+    expect(new Song()).toBeTruthy();
+  });
+});
