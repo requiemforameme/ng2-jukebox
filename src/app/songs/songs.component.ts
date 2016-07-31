9b17d2ef17db03eb356a5a49f7666b0822dcad62
git <briant.dev@gmail.com>
2016-07-31 16:13:43 -0500

    baseline songs component

---
+++ src/app/songs/songs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-songs',
+  templateUrl: 'songs.component.html',
+  styleUrls: ['songs.component.css']
+})
+export class SongsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
