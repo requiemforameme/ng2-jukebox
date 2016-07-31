88f1330762949df33f4566f26f695fd45ac33b93
git <briant.dev@gmail.com>
2016-07-31 16:14:38 -0500

    updated base models

--- src/app/shared/models/playlist.ts
+++ src/app/shared/models/playlist.ts
@@ -1,3 +1,7 @@
+import { Song } from './song';
+
 export class Playlist {
-  
+  id: number;
+  name: string;
+  songs: Array<Song>;
 }
--- src/app/shared/models/song.ts
+++ src/app/shared/models/song.ts
@@ -1,2 +1,8 @@
 export class Song {
+  name: string;
+  artist: string;
+  album: string;
+  genre: string;
+  track: number;
+  year: number;
 }
